d786a2bf842372ec33d732dded52ef73bfd301ca
moliveiralucas <lucasoliveiramarques@hotmail.com.br>
2018-05-06 12:53:37 -0300

    Remoção de importações que não estão sendo utilizadas

--- server/EasyLabWebService/src/main/java/com/moliveiralucas/EasyLab/servico/LaboratorioServico.java
+++ server/EasyLabWebService/src/main/java/com/moliveiralucas/EasyLab/servico/LaboratorioServico.java
@@ -1,9 +1,5 @@
 package com.moliveiralucas.EasyLab.servico;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
 
 public class LaboratorioServico {
 
