b8f8680fab86c187f9733911139ab269b86f9364
moliveiralucas <lucasoliveiramarques@hotmail.com.br>
2018-05-06 12:52:11 -0300

    Implementação da camada de Serviço de Convenio

--- server/EasyLabWebService/src/main/java/com/moliveiralucas/EasyLab/servico/ConvenioServico.java
+++ server/EasyLabWebService/src/main/java/com/moliveiralucas/EasyLab/servico/ConvenioServico.java
@@ -1,5 +1,54 @@
 package com.moliveiralucas.EasyLab.servico;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
+import com.moliveiralucas.EasyLab.model.Convenio;
+import com.moliveiralucas.EasyLab.negocio.ConvenioNegocio;
+
+@RestController
 public class ConvenioServico {
 
+	ConvenioNegocio mConvenioNegocio = new ConvenioNegocio();
+	Gson mGson = new Gson();
+	
+	@RequestMapping(value = "/cadastrarConvenio/{convenio}",
+			method = RequestMethod.GET,
+			produces = "application/json;charset=UTF-8")
+	public @ResponseBody String cadastrarConvenio(@PathVariable String convenio) {
+		Convenio mConvenio = new Convenio();
+		mConvenio.setConvenio(convenio);
+		return mGson.toJson(mConvenioNegocio.cadastrarConvenio(mConvenio));
+	}
+	
+	@RequestMapping(value = "/alterarConvenio/{id_Convenio}_{convenio}",
+			method = RequestMethod.GET,
+			produces = "application/json;charset=UTF-8")
+	public @ResponseBody String alterarConvenio(@PathVariable String id_Convenio, @PathVariable String convenio) {
+		Convenio mConvenio = new Convenio();
+		mConvenio.setId_Convenio(Integer.parseInt(id_Convenio));
+		mConvenio.setConvenio(convenio);
+		return mGson.toJson(mConvenioNegocio.alterarConvenio(mConvenio));
+	}
+	
+	@RequestMapping(value = "/excluirConvenio/{id_Convenio}_{convenio}",
+			method = RequestMethod.GET,
+			produces = "application/json;charset=UTF-8")
+	public @ResponseBody String excluirConvenio(@PathVariable String id_Convenio, @PathVariable String convenio) {
+		Convenio mConvenio = new Convenio();
+		mConvenio.setId_Convenio(Integer.parseInt(id_Convenio));
+		mConvenio.setConvenio(convenio);
+		return mGson.toJson(mConvenioNegocio.excluirConvenio(mConvenio));
+	}
+	
+	@RequestMapping(value = "/consultarConvenio/{parametroBusca}",
+			method = RequestMethod.GET,
+			produces = "application/json;charset=UTF-8")
+	public @ResponseBody String consultarConvenio(@PathVariable String parametroBusca) {
+		return mGson.toJson(mConvenioNegocio.consultarConvenio(parametroBusca));
+	}
 }
