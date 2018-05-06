95874f2d70e6707a2bb58c799b80860d75797ec1
moliveiralucas <lucasoliveiramarques@hotmail.com.br>
2018-05-06 13:47:08 -0300

    Implementação do serviço de Estados

--- server/EasyLabWebService/src/main/java/com/moliveiralucas/EasyLab/servico/EstadoServico.java
+++ server/EasyLabWebService/src/main/java/com/moliveiralucas/EasyLab/servico/EstadoServico.java
@@ -25,4 +25,33 @@ public class EstadoServico {
 		mEstado.setUf(uf);
 		return mGson.toJson(mEstadoNegocio.cadastrarEstado(mEstado));
 	}
+	
+	@RequestMapping(value = "/alterarEstado/{estado}_{uf}_{id_UF}", 
+			method = RequestMethod.GET, 
+			produces = "application/json;charset=UTF-8")
+	public @ResponseBody String alterarEstado(@PathVariable String estado, @PathVariable String uf, @PathVariable String id_UF) {
+		Estado mEstado = new Estado();
+		mEstado.setEstado(estado);
+		mEstado.setId_Estado(Integer.parseInt(id_UF));
+		mEstado.setUf(uf);
+		return mGson.toJson(mEstadoNegocio.alterarEstado(mEstado));
+	}
+	
+	@RequestMapping(value = "/consultarEstado/{parametroBusca}", 
+			method = RequestMethod.GET, 
+			produces = "application/json;charset=UTF-8")
+	public @ResponseBody String consultarEstado(@PathVariable String parametroBusca) {
+		return mGson.toJson(mEstadoNegocio.consultarEstado(parametroBusca));
+	}
+	
+	@RequestMapping(value = "/cadastrarEstado/{estado}_{uf}_{id_UF}", 
+			method = RequestMethod.GET, 
+			produces = "application/json;charset=UTF-8")
+	public @ResponseBody String excluirEstado(@PathVariable String estado, @PathVariable String uf, @PathVariable String id_UF) {
+		Estado mEstado = new Estado();
+		mEstado.setEstado(estado);
+		mEstado.setId_Estado(Integer.parseInt(id_UF));
+		mEstado.setUf(uf);
+		return mGson.toJson(mEstadoNegocio.excluirEstado(mEstado));
+	}
 }
