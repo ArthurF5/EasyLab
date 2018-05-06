bd84dcee6bbb30f4d393bb934110e83387f929ae
moliveiralucas <lucasoliveiramarques@hotmail.com.br>
2018-05-06 15:06:09 -0300

    Implementação da camada de serviço Laboratorio

--- server/EasyLabWebService/src/main/java/com/moliveiralucas/EasyLab/servico/LaboratorioServico.java
+++ server/EasyLabWebService/src/main/java/com/moliveiralucas/EasyLab/servico/LaboratorioServico.java
@@ -1,6 +1,55 @@
 package com.moliveiralucas.EasyLab.servico;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.Gson;
+import com.moliveiralucas.EasyLab.model.Laboratorio;
+import com.moliveiralucas.EasyLab.negocio.LaboratorioNegocio;
+
+@RestController
 public class LaboratorioServico {
 
+	LaboratorioNegocio mLaboratorioNegocio = new LaboratorioNegocio();
+	Gson mGson = new Gson();
+	
+	@RequestMapping(value="/cadastrarLaboratorio/{laboratorio}",
+			method = RequestMethod.GET,
+			produces = "application/json;charset=UTF-8")
+	public @ResponseBody String cadastrarLaboratorio(@PathVariable String laboratorio) {
+		Laboratorio mLaboratorio = new Laboratorio();
+		mLaboratorio.setLaboratorio(laboratorio);
+		return mGson.toJson(mLaboratorioNegocio.cadastrarLaboratorio(mLaboratorio));
+	}
+	
+	@RequestMapping(value="/alterarLaboratorio/{laboratorio}_{id_Laboratorio}",
+			method = RequestMethod.GET,
+			produces = "application/json;charset=UTF-8")
+	public @ResponseBody String alterarLaboratorio(@PathVariable String laboratorio, @PathVariable String id_Laboratorio) {
+		Laboratorio mLaboratorio = new Laboratorio();
+		mLaboratorio.setId_Laboratorio(Integer.parseInt(id_Laboratorio));
+		mLaboratorio.setLaboratorio(laboratorio);
+		return mGson.toJson(mLaboratorioNegocio.alterarLaboratorio(mLaboratorio));
+	}
+	
+	@RequestMapping(value="/excluirLaboratorio/{laboratorio}_{id_Laboratorio}",
+			method = RequestMethod.GET,
+			produces = "application/json;charset=UTF-8")
+	public @ResponseBody String excluirLaboratorio(@PathVariable String laboratorio, @PathVariable String id_Laboratorio) {
+		Laboratorio mLaboratorio = new Laboratorio();
+		mLaboratorio.setId_Laboratorio(Integer.parseInt(id_Laboratorio));
+		mLaboratorio.setLaboratorio(laboratorio);
+		return mGson.toJson(mLaboratorioNegocio.excluirLaboratorio(mLaboratorio));
+	}
+	
+	@RequestMapping(value="/consultarLaboratorio/{parametroBusca}",
+			method = RequestMethod.GET,
+			produces = "application/json;charset=UTF-8")
+	public @ResponseBody String consultarLaboratorio(@PathVariable String parametroBusca) {
+		return mGson.toJson(mLaboratorioNegocio.consultarLaboratorio(parametroBusca));
+	}
+	
 }
